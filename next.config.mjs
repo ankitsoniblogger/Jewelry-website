ec94be3f8bb0bc5d3919c6ffb9fecb6711e48f9c
Ankit Soni <ankitsoniblogger@gmail.com>
2024-09-03 10:41:12 +0400

    jewelry website with Live API

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+};
+
+
 
 export default nextConfig;
